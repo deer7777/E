7912fa20f0f706205bf6c188724a5f3018656811
congxia wu <1398173038@qq.com>
2023-11-11 18:08:30 +0800

    v2.0.0

--- E.go
+++ E.go
@@ -6,6 +6,6 @@ import (
 )
 
 func E_print() {
-	fmt.Println("version E_v1.5")
+	fmt.Println("version E_v2.0.0")
 	fmt.Println(time.Now())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/deer7777/E
+module github.com/deer7777/E/v2
 
 go 1.21.1
